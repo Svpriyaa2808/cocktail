dfb301fb82bbe8bf145a2c96fc66b3991876a599
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-05-22 12:02:46 +0200

    creating develop branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
- 
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'; 
 import "./globals.css";
 
 const geistSans = Geist({
