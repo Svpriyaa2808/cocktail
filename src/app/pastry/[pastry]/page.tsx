15e63c3c7cfeef68a413ca5b8623596befdc96c4
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-05-26 18:38:56 +0200

    adding changes

--- src/app/pastry/[pastry]/page.tsx
+++ src/app/pastry/[pastry]/page.tsx
@@ -1,21 +1,34 @@
 import { selectedMenu } from "@/data/data"
 
-const RecipePage =  ({params}: {params:{pastry:string}}) =>{ 
-    const pastry =  decodeURIComponent(params.pastry);
 
-        // const slugify = (text: string):string => {
-        //     return text.toLowerCase().replace(/\s+/g, '-')           // Replace spaces with -       // Remove all non-word chars
+
+// const RecipePage =  ({params}: {params:{pastry:string}}) =>{ 
+//     const pastry =  decodeURIComponent(params.pastry);
+
+//         // const slugify = (text: string):string => {
+//         //     return text.toLowerCase().replace(/\s+/g, '-')           // Replace spaces with -       // Remove all non-word chars
            
-        //    }
+//         //    }
         
-        //   console.log("hi")
-        //  
-        //   const slug = slugify(pastry) 
-        //   console.log(selectedMenu(params.replace(/\s+/g, '-')))
+//         //   console.log("hi")
+//         //  
+//         //   const slug = slugify(pastry) 
+//         //   console.log(selectedMenu(params.replace(/\s+/g, '-')))
          
-    return(
-        <p>{pastry}</p>
-    )
-}
+//     return(
+//         <p>{pastry}</p>
+//     )
+// }
 
-export default RecipePage
+// export default RecipePage
+
+type Props = {
+    params: {
+      pastry: string;
+    };
+  };
+  
+  export default function Page({ params }: Props) {
+    return <div>Pastry: {params.pastry}</div>;
+  }
+  
